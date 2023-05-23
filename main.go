ff6da9f65ab41a58512855c50b5df397295e12f1
babybabycloud <364847242@qq.com>
2023-05-23 07:28:45 +0000

    Prepare for publish
    Prepare for publish

--- main.go
+++ main.go
@@ -16,6 +16,6 @@ func main() {
 		// provider address is used in these tutorials in conjunction with a
 		// specific Terraform CLI configuration for manual development testing
 		// of this provider.
-		Address: "hashicorp.com/edu/unarchive",
+		Address: "registry.terraform.io/babybabycloud/unarchive",
 	})
 }
